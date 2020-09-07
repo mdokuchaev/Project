734ebaacef0b0e4cdd42d77f4a15248ff7ee74eb
mdokuchaev <70489505+mdokuchaev@users.noreply.github.com>
2020-09-07 19:12:10 +0300

    Update Project2_10.java

--- src/ru/x5/Project2_10.java
+++ src/ru/x5/Project2_10.java
@@ -1,4 +1,11 @@
-package ru.x5;
+/*
+
+Дан массив целых чисел: [15,10,0,-6,-5,3,0,-34,0,32,56,0,24,-52].
+Переместить нули в конец массива. Вывести результат в консоль.
+
+*/
+
+//package ru.x5;
 
 import java.util.Arrays;
 
@@ -10,30 +17,20 @@ public class Project2_10 {
        System.out.println("Массив целых чисел: " +  Arrays.toString(mass));
    
        int count = 0;
-       int temp = mass.length;
-       
-       for (int i=0; i < mass.length; i++) {
-           
+ 
+       for (int i = 0; i < mass.length; i++) {
            if (mass[i] != 0) {
                mass[count++] = mass[i];
-              
-               System.out.println("A " +  Arrays.toString(mass));
-           } else {
-               temp--;
-           }
-           
-                              
-           if (i >= temp) {
-                mass[i] = 0;
-                System.out.println("B " +  Arrays.toString(mass) + " i - " + i + " count - " + count+ " temp - " + temp);
-           }
-           
-           
+           } 
        }
+       
+       for (int i = count; i < mass.length; i++) {
+           mass[i] = 0; 
+       }
+       
        
        System.out.println("Массив целых чисел - нули в конце масива: " +  Arrays.toString(mass));
      
    }
  
 }
-
