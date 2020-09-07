f4d21ddcd2e3d28773760684ab3bd29a6b0d17bc
mdokuchaev <70489505+mdokuchaev@users.noreply.github.com>
2020-09-07 16:17:13 +0300

    Update Project2_9.java

--- src/ru/x5/Project2_9.java
+++ src/ru/x5/Project2_9.java
@@ -1,3 +1,10 @@
+/*
+Дан массив целых чисел: [15,10,51,-6,-5,3,-10,-34,0,32,56,-12,24,-52].
+
+Переставить элементы массива в обратном порядке.
+Вывести результат в консоль
+*/
+
 package ru.x5;
 
 import java.util.Arrays;
@@ -11,7 +18,7 @@ public class Project2_9 {
    
        int a = 0;
        
-       for (int i=0; i < mass.length / 2; i++) {
+       for (int i = 0; i < mass.length / 2; i++) {
            
            a = mass [i];
            mass [i] = mass [mass.length - 1 - i];
