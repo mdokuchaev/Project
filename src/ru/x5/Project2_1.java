ddb5644193e32da64917d57094ffe327242d7efe
mdokuchaev <70489505+mdokuchaev@users.noreply.github.com>
2020-09-09 20:12:57 +0300

    Update Project2_1.java

--- src/ru/x5/Project2_1.java
+++ src/ru/x5/Project2_1.java
@@ -4,26 +4,25 @@
 
 package ru.x5;
 
-import java.util.Scanner;
+import java.util.Scanner; 
 
-public class Project2_1 {
+public class Project2_1 { 
+      
+    public static void main(String[] args) { 
+      
+     Scanner in = new Scanner(System.in); 
     
-   public static void main(String[] args) {
-    
-    Scanner in = new Scanner(System.in);
-    long i = 0;
-
-    System.out.print("Введите целое число: ");
-    String s = in.nextLine();
-   
-    try {  
-        i = Integer.parseInt(s);
-        System.out.println("Последняя цифра введенного числа: " + Math.abs(i % 10));
-    } 
-    catch (NumberFormatException e) {
-        System.out.println("Ошибка: " + s + " не является целым числом.");
-    }
-    in.close();
-}
+     System.out.print("Введите целое число: "); 
+     String s = in.nextLine(); 
+     
+     try {   
  
-}
+         System.out.println("Последняя цифра введенного числа: " + Math.abs(Integer.parseInt(s) % 10)); 
+     }  
+     catch (NumberFormatException e) { 
+         System.out.println("Ошибка: " + s + " не является целым числом."); 
+     } 
+     in.close(); 
+ } 
+   
+ } 
